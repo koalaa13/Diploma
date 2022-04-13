d4f9f7b12650c40ff45bb47730300f196becfb91
koalaa13 <nooshe981@gmail.com>
2022-04-13 20:59:35 +0300

    changed Estimator: < 0 = null, checking by dist to center of cluster now

--- estimator/Estimator.py
+++ estimator/Estimator.py
@@ -4,7 +4,6 @@ import sys
 
 import optuna
 import torch
-import torch.nn.functional as F
 from optuna import Trial
 from optuna.samplers import TPESampler
 from optuna.samplers._tpe.sampler import _get_observation_pairs, _split_observation_pairs
@@ -16,20 +15,21 @@ from embedding.graph import NeuralNetworkGraph
 
 class Estimator:
     @staticmethod
-    def __get_param_name(i, j, suffix):
-        return "x_" + str(i) + "_" + str(j) + "_" + suffix
+    def __get_param_name(i, j):
+        return "x_" + str(i) + "_" + str(j)
 
-    def __init__(self, embedding_width, embedding_height, train_dataloader, test_dataloader, optimizer, loss):
+    def __init__(self, embedding_width, embedding_height, train_dataloader, test_dataloader, loss):
         self.inf = math.inf
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
-        self.optimizer = optimizer
         self.loss = loss
+        self.good_center = [0.0] * embedding_width
+        self.bad_center = [0.0] * embedding_width
 
         self.embedding_width = embedding_width
         self.embedding_height = embedding_height
         self.study = self.pre_train()
-        values, scores = _get_observation_pairs(self.study, [self.__get_param_name(0, 0, "null")], False, False)
+        values, scores = _get_observation_pairs(self.study, [self.__get_param_name(0, 0)], False, False)
         indices_below, indices_above = _split_observation_pairs(scores, self.study.sampler._gamma(len(scores)))
         trials = self.study.get_trials()
         self.points = []
@@ -39,10 +39,8 @@ class Estimator:
             for i in range(self.embedding_height):
                 cur_line = []
                 for j in range(self.embedding_width):
-                    if trials[ind].params[self.__get_param_name(i, j, "null")] == "null":
-                        cur_line.append(None)
-                    else:
-                        cur_line.append(float(trials[ind].params[self.__get_param_name(i, j, "")]))
+                    cur_line.append(float(trials[ind].params[self.__get_param_name(i, j)]))
+                    self.good_center[j] += cur_line[j]
                 cur_point.append(cur_line)
             self.points.append((cur_point, True))
         for ind in indices_above:
@@ -50,12 +48,15 @@ class Estimator:
             for i in range(self.embedding_height):
                 cur_line = []
                 for j in range(self.embedding_width):
-                    if trials[ind].params[self.__get_param_name(i, j, "null")] == "null":
-                        cur_line.append(None)
-                    else:
-                        cur_line.append(float(trials[ind].params[self.__get_param_name(i, j, "")]))
+                    cur_line.append(float(trials[ind].params[self.__get_param_name(i, j)]))
+                    self.bad_center[j] += cur_line[j]
                 cur_point.append(cur_line)
             self.points.append((cur_point, False))
+        good_cnt = len(indices_below)
+        bad_cnt = len(indices_above)
+        for i in range(self.embedding_width):
+            self.good_center[i] /= good_cnt
+            self.bad_center[i] /= bad_cnt
 
     def objective(self, trial: Trial):
         n = trial.suggest_int("embedding_height", 1, self.embedding_height)
@@ -64,17 +65,15 @@ class Estimator:
         for i in range(n):
             cur_line = []
             for j in range(m):
-                # TODO decide this values
-                # TODO low and high should be chosen with a margin cuz optuna doesn't search in bounds area
-                low = -100.0
-                high = 100.0
-                step = 0.1
-                is_null = trial.suggest_categorical(self.__get_param_name(i, j, "null"), ["null", "not_null"])
-                if is_null == "null":
-                    x_i = None
-                else:
-                    x_i = trial.suggest_float(self.__get_param_name(i, j, ""), low, high, step=step)
+                left = -1.
+                right = 1000.
+                step = 1e-5
+                # left border respond for null probability cuz
+                # null = x_i < 0
                 # mb use another distribution for parameters of embedding
+                x_i = trial.suggest_float(self.__get_param_name(i, j), left, right, step=step)
+                if x_i < 0.0:
+                    x_i = None
                 cur_line.append(x_i)
             embedding.append(cur_line)
         return self.get_quality_from_embedding(embedding)
@@ -82,6 +81,7 @@ class Estimator:
     # TODO this function has to create network from embedding and return some metrics of quality
     # TODO for example accuracy
     def get_quality_from_embedding(self, embedding):
+        # generate a file with Pytorch realization of embedded network
         graph = NeuralNetworkGraph.get_graph(embedding)
         filepath = 'tmp/tmp.py'
         model_name = 'Tmp'
@@ -97,14 +97,15 @@ class Estimator:
             model.train()
             n_epoch = 10
 
+            # mb configure optimizer here
+            optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
             for epoch in range(n_epoch):
                 for data, target in self.train_dataloader:
-                    self.optimizer.zero_grad()
+                    optimizer.zero_grad()
                     output = model(data)
                     loss = self.loss(output, target)
                     loss.backward()
-                    # mb should re-create optimizer for every invocation of get_quality_from_embedding method
-                    self.optimizer.step()
+                    optimizer.step()
 
             # testing and calculating accuracy
             model.eval()
@@ -147,16 +148,4 @@ class Estimator:
         return math.sqrt(res)
 
     def check(self, embedding):
-        # take some radius for k nearest neighbours method
-        r = 1337
-        count_near_good = 0
-        count_near_bad = 0
-        for p in self.points:
-            dist = self.__metrics(embedding, p)
-            if dist <= r:
-                # is good
-                if p[1]:
-                    count_near_good += 1
-                else:
-                    count_near_bad += 1
-        return count_near_good > count_near_bad
+        return self.__metrics(self.good_center, embedding) > self.__metrics(self.bad_center, embedding)
